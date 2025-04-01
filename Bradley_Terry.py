45fa00bf7a8d270627d2ff7c327c11f867c33248
refouch <remi.foucherand@ensae.fr>
2025-04-01 14:44:17 +0200

    ajout d'une classe pour les fomrules du tournoi

---
+++ Bradley_Terry.py
@@ -0,0 +1,32 @@
+####
+# Définition d'un classe pour simuler un tournoi
+####
+import numpy as np
+
+def BT_formula(Vi,Vj):
+    return Vi/(Vi+Vj)
+
+class BT_Tournament:
+
+    def __init__(self, strength):
+        self.strength = strength
+
+    def compute_probs(self,i,j):
+        Vi = self.strength[i]
+        Vj = self.strength[j]
+        return BT_formula(Vi,Vj)
+
+    def score(self,i):
+        Score = 0
+        for j in range(len(self.strength)):
+            if i != j:
+                proba = self.compute_probs(i,j)
+                Score += np.random.binomial(1,proba)
+        return Score
+
+    def winner(self):
+        Scores = []
+        for i in range(len(self.strength)):
+            Scores.append(self.score(i))
+        winner = Scores.index(max(Scores))
+        return winner
