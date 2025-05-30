5c17ad4cac38796d1d82f1a37cb5d3db9552041e
Leo DONY <leo.dony@orange.fr>
2025-05-30 12:18:39 +0200

    Graphiques thm 1

--- main_simu.py
+++ main_simu.py
@@ -1,4 +1,29 @@
-from code_claude import*
+from Verif_thm1 import*
 
-print(Plot_proba_optimisee(scale=0.2, n_values=[100,500,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000],methode='theorique'))
-'''print(Plot_proba_optimisee(scale=0.2,n_values=None))'''
+n_simulation = [50,100,250,500,750,1000,2000,3000,4000,5000]
+n_theorique = [100,500,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+
+####### Loi exponentielle : Exp(5) #######
+
+# Version simulée
+'''print(Plot_proba_optimisee(distribution='exponentielle', methode='parallele',n_values=n_simulation))'''
+
+# Version théorique
+'''print(Plot_proba_optimisee(distribution='exponentielle', methode='theorique',n_values=n_theorique))'''
+
+
+####### Loi Uniforme : U([0,1]) #######
+
+# Version simulée
+'''print(Plot_proba_optimisee(distribution='uniforme', methode='parallele',n_values=n_simulation))'''
+
+# Version théorique
+'''print(Plot_proba_optimisee(distribution='uniforme', methode='theorique',n_values=n_theorique))'''
+
+
+####### Loi Beta : Beta(1,5) #######
+
+# Version simulée
+print(Plot_proba_optimisee(distribution='beta', methode='parallele',n_values=n_simulation))
+# Version théorique
+'''print(Plot_proba_optimisee(distribution='beta', methode='theorique',n_values=n_theorique))'''
